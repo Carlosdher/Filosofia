90d3af4fd15bb16388eef2adc178fb77854c1c25
Carlosdher <carlosabc436@gmail.com>
2020-04-17 09:28:01 -0300

    desenvolvimento de telas

--- App/core/models.py
+++ App/core/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import uuid
+
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser, Group, Permission
@@ -9,8 +11,6 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 # ---- Base model ------
 
 class CreatePost(models.Model):
-
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     post_date = models.DateTimeField('Postado', auto_now_add=True)
     set_date = models.DateTimeField('atualizado', auto_now=True)
-
-
